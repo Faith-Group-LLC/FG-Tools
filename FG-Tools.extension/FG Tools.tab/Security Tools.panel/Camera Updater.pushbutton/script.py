21c2580a6820431bc9abb5de05fa4899d23763b0
Hayden Fulghum <hayden.fulghum@faithgroupllc.com>
2025-03-14 13:33:59 -0700

    Fix import statement and format select camera dialog in Security Tools panel scripts

--- FG-Tools.extension/FG Tools.tab/Security Tools.panel/Camera Updater.pushbutton/script.py
+++ FG-Tools.extension/FG Tools.tab/Security Tools.panel/Camera Updater.pushbutton/script.py
@@ -161,10 +161,10 @@ camsInProject = camera_Collector()
 ops = [listOption(element) for element in camsInProject]
 
 select_cam = forms.SelectFromList.show(ops,
-                                       title='Select Camera to Modify'
+                                       title='Select Camera to Modify',
                                        multiselect=False,
                                        button_name='Select Camera'
-                                       ) 
+                                       )
 
 print(print_single_camdata(select_cam))
 
